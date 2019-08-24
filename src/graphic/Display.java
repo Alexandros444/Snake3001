08ad764770d9286dc4f2c3a8510ddf4a094dd6a0
Alexandros444 <alexmohr0404@gmx.de>
2019-08-24 15:47:03 +0200

    Display-Klasse erstellt

---
+++ src/graphic/Display.java
@@ -0,0 +1,58 @@
+package graphic;
+
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Klasse zum erstellen eines Displays, hier wird unser Spiel angezeigt!
+ * 
+ * @author Alex diktiert von Ben(Dem Diktator)
+ */
+
+public class Display {
+	private long windowID;
+
+	/**
+	 * Erstellt neues Display mit der Breite W, der Höhe H, und dem Titel titel
+	 * 
+	 * @param w Breite
+	 * @param h Höhe
+	 * @param title Titel
+	 *
+	 */
+
+	public Display(int w, int h, String title) {
+		if (!GLFW.glfwInit()) {
+			throw new IllegalStateException("GLFW kaputt");
+		}
+		windowID = GLFW.glfwCreateWindow(w, h, title, 0, 0);
+		if (windowID == 0) {
+			throw new IllegalStateException("Fenster kaputt");
+		}
+	}
+
+	/**
+	 * Updated das Display und checkt die Events
+	 */
+	
+	public void update() {
+		GLFW.glfwPollEvents();
+		
+	}
+	
+	/**
+	 * @return Ob das Fenster geschlossen werden soll
+	 */
+
+	public boolean isCloseRequested() {
+		return GLFW.glfwWindowShouldClose(windowID);
+	}
+	
+	/**
+	 * Schließt das Fenster 
+	 */
+	
+	public void close() {
+		GLFW.glfwDestroyWindow(windowID);
+		GLFW.glfwTerminate();
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,30 @@
 package main;
 
+import graphic.Display;
+
 /**
  * Die Klasse mit der Main-Methode unseres Programms.<br>
  * 
  * Bisher nur als Beispiel für den Umgang mit Eclipse, Git  und Javadoc gedacht.
  * 
- * @author Ben Eltschig
+ * @author Ben
+ * @author Alex
  */
 
 public class Main {
 	
 	/**
-	 * Die Main-Methode selbst. Sie gibt "Hello World!" aus.
+	 * Die Main-Methode selbst. Sie öffnet ein Fenster.
 	 * 
 	 * @param args Kommandozeilenparameter
 	 */
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Display display = new Display(960,540,"TITEL");
+		while(!display.isCloseRequested()) {
+			display.update();
+		}
+		display.close();
 	}
 	
 }
