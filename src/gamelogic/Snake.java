a842ac32da162318d0abf00d2edfde7a059ec3c7
Jakopokaj <opokajsaerdna@gmail.com>
2019-09-16 14:54:09 +0200

    neues icon eingefügt

--- src/gamelogic/Snake.java
+++ src/gamelogic/Snake.java
@@ -34,53 +34,53 @@ public class Snake {
 	// Updated die Bewegung der Schlange
 	public void update(Display display) {
 		if(isAlive==true) {
-		// dreht die Sichtmatrix je nach Tasteninput und lädt sie in den Shader
-	
-		// dreht Sichtmatrix nach oben					
-		if (display.isKeyPressed(GLFW.GLFW_KEY_W) || display.isKeyPressed(GLFW.GLFW_KEY_UP) ) {
-			viewDirection.rotate(-rotationSpeed, 0, 0);
-		}
-		// dreht Sichtmatrix nach unten
-		if (display.isKeyPressed(GLFW.GLFW_KEY_S) || display.isKeyPressed(GLFW.GLFW_KEY_DOWN)) {
-			viewDirection.rotate(rotationSpeed, 0, 0);
-		}
-		// dreht Sichtmatrix nach links
-		if (display.isKeyPressed(GLFW.GLFW_KEY_A) || display.isKeyPressed(GLFW.GLFW_KEY_LEFT)) {
-			viewDirection.rotate(0, -rotationSpeed, 0);
-		}
-		// Dreht Sichtmatrix nach rechts 
-		if (display.isKeyPressed(GLFW.GLFW_KEY_D) || display.isKeyPressed(GLFW.GLFW_KEY_RIGHT)) {
-			viewDirection.rotate(0, rotationSpeed, 0);
-		}
-		// Rotiert Sichtmatrix nach links
-		if (display.isKeyPressed(GLFW.GLFW_KEY_Q)) {
-			viewDirection.rotate(0, 0, rotationSpeed);
-		}
-		// Rotiert Sichtmatrix nach rechts
-		if (display.isKeyPressed(GLFW.GLFW_KEY_E)) {
-			viewDirection.rotate(0, 0, -rotationSpeed);
-		}
-		//wenn Leertaste gedrückt dann stop
-		if (isAlive==true && !display.isKeyPressed(GLFW.GLFW_KEY_SPACE)) {
-			// Setzt den BewegungsVektor zurück
-			movement.x = 0;
-			movement.y = 0;
-			movement.z = movementSpeed;
-			// dreht den BewegungsVektor durch die SichtMatrix
-			movement.apply(viewDirection);
-			// addiert den BewegungsVektor zum Kamera-Positions-Vektor 
-			cameraPosition.add(movement);
-		}
-	
-		// ruft die Methode zum Updaten der Positionen auf
-		updateSnakePositions(); 
+			// dreht die Sichtmatrix je nach Tasteninput und lädt sie in den Shader
 		
-		//checkt für jede Kugel ob man kollidiert
-		for(int i=2;i<snakePositions.length;i++) {
-			if(sphereDistance(snakePositions[0],snakePositions[i])<0) {
-				isAlive=false;
+			// dreht Sichtmatrix nach oben					
+			if (display.isKeyPressed(GLFW.GLFW_KEY_W) || display.isKeyPressed(GLFW.GLFW_KEY_UP) ) {
+				viewDirection.rotate(-rotationSpeed, 0, 0);
 			}
-		}
+			// dreht Sichtmatrix nach unten
+			if (display.isKeyPressed(GLFW.GLFW_KEY_S) || display.isKeyPressed(GLFW.GLFW_KEY_DOWN)) {
+				viewDirection.rotate(rotationSpeed, 0, 0);
+			}
+			// dreht Sichtmatrix nach links
+			if (display.isKeyPressed(GLFW.GLFW_KEY_A) || display.isKeyPressed(GLFW.GLFW_KEY_LEFT)) {
+				viewDirection.rotate(0, -rotationSpeed, 0);
+			}
+			// Dreht Sichtmatrix nach rechts 
+			if (display.isKeyPressed(GLFW.GLFW_KEY_D) || display.isKeyPressed(GLFW.GLFW_KEY_RIGHT)) {
+				viewDirection.rotate(0, rotationSpeed, 0);
+			}
+			// Rotiert Sichtmatrix nach links
+			if (display.isKeyPressed(GLFW.GLFW_KEY_Q)) {
+				viewDirection.rotate(0, 0, rotationSpeed);
+			}
+			// Rotiert Sichtmatrix nach rechts
+			if (display.isKeyPressed(GLFW.GLFW_KEY_E)) {
+				viewDirection.rotate(0, 0, -rotationSpeed);
+			}
+			//wenn Leertaste gedrückt dann stop
+			if (isAlive==true && !display.isKeyPressed(GLFW.GLFW_KEY_SPACE)) {
+				// Setzt den BewegungsVektor zurück
+				movement.x = 0;
+				movement.y = 0;
+				movement.z = movementSpeed;
+				// dreht den BewegungsVektor durch die SichtMatrix
+				movement.apply(viewDirection);
+				// addiert den BewegungsVektor zum Kamera-Positions-Vektor 
+				cameraPosition.add(movement);
+			}
+		
+			// ruft die Methode zum Updaten der Positionen auf
+			updateSnakePositions(); 
+			
+			//checkt für jede Kugel ob man kollidiert
+			for(int i=2;i<snakePositions.length;i++) {
+				if(sphereDistance(snakePositions[0],snakePositions[i])<0) {
+					isAlive=false;
+				}
+			}
 		}
 	}
 
