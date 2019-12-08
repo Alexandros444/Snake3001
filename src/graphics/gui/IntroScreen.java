c58025fa910257d4bb7ba1bc40384733c8189d69
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2019-12-08 20:25:48 +0100

    Github-Link in IntroScreen

--- src/graphics/gui/IntroScreen.java
+++ src/graphics/gui/IntroScreen.java
@@ -2,6 +2,9 @@ package graphics.gui;
 
 import graphics.gui.engine.components.BoxComponent;
 import graphics.gui.engine.components.ImageComponent;
+import graphics.gui.engine.components.TextComponent;
+import graphics.gui.engine.fonts.Font;
+import graphics.gui.engine.fonts.MonospaceFont;
 import util.Timer;
 
 /**
@@ -14,6 +17,8 @@ public class IntroScreen extends BoxComponent{
 	private Timer timer;
 	private ImageComponent image;
 	
+	private Font font;
+	
 	public IntroScreen() {
 		super(0,0,0xffddeeee,0,0);
 		super.setPosition(POSITION_FULL);
@@ -22,6 +27,14 @@ public class IntroScreen extends BoxComponent{
 		image.setPosition(POSITION_CENTER);
 		super.addComponent(image);
 		timer = new Timer();
+		
+		font = new MonospaceFont("res/font/ascii_dark.png");
+		TextComponent text = new TextComponent("Github.com/Alexandros444/Snake3001",font);
+		text.setPosition(POSITION_CORNER_BOTTOMLEFT);
+		text.setOffset(6,6);
+		text.setScale(2);
+		text.setTransparency(0.5f);
+		super.addComponent(text);
 	}
 	
 	public void update() {
@@ -33,6 +46,11 @@ public class IntroScreen extends BoxComponent{
 	
 	public boolean isFinished() {
 		return timer.getTime()>3;
+	}
+	
+	public void destroy() {
+		font.destroy();
+		super.destroy();
 	}
 	
 }
